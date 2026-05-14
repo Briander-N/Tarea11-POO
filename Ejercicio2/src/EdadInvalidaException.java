83dc17b31239aea7bcdf604403dd622c85021ad3
Briander <pochitobriander@gmail.com>
2026-05-14 15:25:29 -0500

    Clase EdadInvalidaException creada

---
+++ Ejercicio2/src/EdadInvalidaException.java
@@ -0,0 +1,17 @@
+public class EdadInvalidaException extends Exception{
+
+    private int edadIngresada;
+
+    public EdadInvalidaException(int edadIngresada){
+        super("Error: la edad ingresada (" + edadIngresada + ") no es válida.");
+        this.edadIngresada = edadIngresada;
+
+    }
+
+    public int getEdadIngresada() {
+
+        return edadIngresada;
+
+    }
+    
+}
