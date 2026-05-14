b252cad6aa5e62d498a8c494707d58e81a118b26
Briander <pochitobriander@gmail.com>
2026-05-14 15:19:27 -0500

    Ejercicio 1 terminado!! Siii

--- Ejercicio1/src/Main.java
+++ Ejercicio1/src/Main.java
@@ -1,7 +1,35 @@
+//Se Importa Scanner
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    //Se inicializa scanner como SC
+    Scanner sc = new Scanner(System.in);
 
+    try {
+        String entrada;
+        double resultado;
+        int numero;
 
+        //Aqui se solicita el numero al usuario
+        System.out.println("Ingrese un numero entero: ");
+        entrada = sc.nextLine();
+
+        //Convertir de texto a numero entero
+        numero = Integer.parseInt(entrada);
+
+        //Division
+        resultado = 100 / numero;
+
+        System.out.println("Resultado: " + resultado);
+
+    } catch (ArithmeticException e){
+        //Aqui da error por dividir por 0
+        System.out.println("No es posible dividir por 0");
+    } catch (NumberFormatException e){
+        //Ocurre si se escribe texto y no un numero
+        System.out.println("Ingrese un numero valido!!!!");
+    } finally{
+        System.out.println("Programada finalizado correctamente!");
+        sc.close();
+    }
 }
